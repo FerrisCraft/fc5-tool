eb708528732ccf8b3e65e0a9d2bfc19db9e484af
Wim Looman <git@nemo157.com>
2023-07-20 19:31:09 +0200

    Initial commit

---
+++ src/data.rs
@@ -0,0 +1,336 @@
+use camino::{Utf8Path, Utf8PathBuf};
+use eyre::{ensure, Context, ContextCompat, Error, Result};
+use std::{
+    fmt::{Debug, Display},
+    str::FromStr,
+};
+
+pub type Compound = std::collections::HashMap<String, fastnbt::Value>;
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Coord<T> {
+    pub x: T,
+    pub z: T,
+}
+
+impl Coord<i64> {
+    #[culpa::throws]
+    #[tracing::instrument]
+    fn from_region_file(name: &str) -> Self {
+        let mut it = name.split('.');
+        ensure!(it.next() == Some("r"), "missing `r` segment");
+        let x = it
+            .next()
+            .context("missing coordinate")
+            .and_then(|x| Ok(x.parse()?))
+            .context("reading x coordinate")?;
+        let z = it
+            .next()
+            .context("missing coordinate")
+            .and_then(|z| Ok(z.parse()?))
+            .context("reading z coordinate")?;
+        ensure!(it.next() == Some("mca"), "missing `mca` segment");
+        ensure!(it.next().is_none(), "extra data");
+        Self { x, z }
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip(self), fields(self = %self))]
+    fn checked_mul(self, value: i64) -> Self {
+        Self {
+            x: self.x.checked_mul(value).context("out of range")?,
+            z: self.z.checked_mul(value).context("out of range")?,
+        }
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip(self, other), fields(self = %self, other = %other))]
+    fn checked_add(self, other: Self) -> Self {
+        Self {
+            x: self.x.checked_add(other.x).context("out of range")?,
+            z: self.z.checked_add(other.z).context("out of range")?,
+        }
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip(self, other), fields(self = %self, other = %other))]
+    fn checked_sub(self, other: Self) -> Self {
+        Self {
+            x: self.x.checked_sub(other.x).context("out of range")?,
+            z: self.z.checked_sub(other.z).context("out of range")?,
+        }
+    }
+
+    pub fn chunk_to_region(self) -> Self {
+        Self {
+            x: self.x >> 5,
+            z: self.z >> 5,
+        }
+    }
+}
+
+impl<T> Coord<T> {
+    #[culpa::throws]
+    #[tracing::instrument(skip(other), fields(other = %other))]
+    fn try_from<U: Display>(other: Coord<U>) -> Self
+    where
+        T: TryFrom<U>,
+        T::Error: std::error::Error + Send + Sync + 'static,
+    {
+        Self {
+            x: other.x.try_into()?,
+            z: other.z.try_into()?,
+        }
+    }
+}
+
+impl<T: FromStr> FromStr for Coord<T>
+where
+    T::Err: std::error::Error + Send + Sync + 'static,
+{
+    type Err = Error;
+
+    #[culpa::throws]
+    #[tracing::instrument]
+    fn from_str(s: &str) -> Self {
+        let mut it = s.split(',');
+        let x = it
+            .next()
+            .context("missing coordinate")
+            .and_then(|x| Ok(x.parse()?))
+            .context("reading x coordinate")?;
+        let z = it
+            .next()
+            .context("missing coordinate")
+            .and_then(|z| Ok(z.parse()?))
+            .context("reading z coordinate")?;
+        ensure!(it.next().is_none(), "extra data");
+        Self { x, z }
+    }
+}
+
+impl<T: Display> Display for Coord<T> {
+    #[culpa::throws(std::fmt::Error)]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) {
+        let Self { x, z } = self;
+        write!(f, "{x},{z}")?;
+    }
+}
+
+#[derive(Debug)]
+pub struct World {
+    pub directory: Utf8PathBuf,
+}
+
+#[culpa::throws]
+#[tracing::instrument]
+pub fn read_level(path: &Utf8Path) -> Compound {
+    use std::io::Read;
+    let mut data = Vec::with_capacity(4096);
+    flate2::read::GzDecoder::new(std::fs::File::open(path)?).read_to_end(&mut data)?;
+    fastnbt::from_bytes(&data)?
+}
+
+#[culpa::throws]
+#[tracing::instrument(skip(value))]
+pub fn write_level(path: &Utf8Path, value: Compound) {
+    use std::io::Write;
+    flate2::write::GzEncoder::new(std::fs::File::create(path)?, flate2::Compression::best())
+        .write_all(&fastnbt::to_bytes(&value)?)?;
+}
+
+impl World {
+    pub fn new(directory: Utf8PathBuf) -> Self {
+        Self { directory }
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip_all, fields(self.directory = %self.directory, coord = %coord))]
+    pub fn region(&self, coord: Coord<i64>) -> Option<Region> {
+        let Coord { x, z } = coord;
+        let path = self.directory.join("region").join(format!("r.{x}.{z}.mca"));
+        Region::from_path(path)?
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip_all, fields(self.directory = %self.directory))]
+    pub fn regions(&self) -> impl Iterator<Item = Result<Region>> {
+        std::fs::read_dir(self.directory.join("region"))
+            .context("reading region dir")?
+            .filter_map(|entry| {
+                entry
+                    .context("reading dir entry")
+                    .and_then(|entry| Ok(Region::from_path(entry.path().try_into()?)?))
+                    .transpose()
+            })
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip_all, fields(self.directory = %self.directory, coord = %coord))]
+    pub fn remove_region(&self, coord: Coord<i64>) {
+        let Coord { x, z } = coord;
+        let path = self.directory.join("region").join(format!("r.{x}.{z}.mca"));
+        match std::fs::remove_file(path) {
+            Ok(()) => Ok(()),
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(()),
+            res => res,
+        }?
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip_all, fields(self.directory = %self.directory, coord = %coord))]
+    pub fn region_for_chunk(&self, coord: Coord<i64>) -> Option<Region> {
+        self.region(Coord {
+            x: coord.x >> 5,
+            z: coord.z >> 5,
+        })?
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip_all, fields(self.directory = %self.directory))]
+    pub fn update_level(&self, mut f: impl FnMut(Compound) -> Result<Compound>) {
+        let path = self.directory.join("level.dat");
+        write_level(&path, f(read_level(&path)?)?)?;
+    }
+}
+
+pub struct Region {
+    pub coord: Coord<i64>,
+    pub path: Utf8PathBuf,
+    region: fastanvil::Region<std::fs::File>,
+}
+
+impl Debug for Region {
+    #[culpa::throws(std::fmt::Error)]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) {
+        f.debug_struct("Region")
+            .field("coord", &self.coord)
+            .field("path", &self.path)
+            .finish()?
+    }
+}
+
+#[culpa::throws]
+#[tracing::instrument(skip_all, fields(region_coord = %region_coord, absolute_chunk_coord = %absolute_chunk_coord))]
+fn make_relative(region_coord: Coord<i64>, absolute_chunk_coord: Coord<i64>) -> Coord<usize> {
+    Coord::<usize>::try_from(absolute_chunk_coord.checked_sub(region_coord.checked_mul(32)?)?)?
+}
+
+#[culpa::throws]
+#[tracing::instrument(skip_all, fields(region_cord = %region_coord, relative_chunk_coord = %relative_chunk_coord))]
+fn make_absolute(region_coord: Coord<i64>, relative_chunk_coord: Coord<usize>) -> Coord<i64> {
+    Coord::<i64>::try_from(relative_chunk_coord)?.checked_add(region_coord.checked_mul(32)?)?
+}
+
+impl Region {
+    #[culpa::throws]
+    #[tracing::instrument]
+    fn from_path(path: Utf8PathBuf) -> Option<Self> {
+        match std::fs::metadata(&path) {
+            Ok(metadata) if metadata.len() == 0 => return None,
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => return None,
+            res => res,
+        }?;
+        let coord = Coord::from_region_file(path.file_name().context("missing filename")?)?;
+        let file = std::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .open(&path)
+            .context("opening region file")?;
+        let region = fastanvil::Region::from_stream(file).context("initializing region")?;
+        Some(Self {
+            coord,
+            path,
+            region,
+        })
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip_all, fields(self.path = %self.path, self.coord = %self.coord, chunk.relative_coord = %chunk.relative_coord))]
+    fn save_chunk(&mut self, chunk: &Chunk) {
+        self.region.write_chunk(
+            chunk.relative_coord.x,
+            chunk.relative_coord.z,
+            &chunk.serialize()?,
+        )?;
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip_all, fields(self.path = %self.path, self.coord = %self.coord, absolute_coord = %absolute_coord))]
+    pub fn update_chunk(
+        &mut self,
+        absolute_coord: Coord<i64>,
+        mut f: impl FnMut(Chunk) -> Result<Chunk>,
+    ) {
+        let relative_coord = make_relative(self.coord, absolute_coord)?;
+        let data = self
+            .region
+            .read_chunk(relative_coord.x, relative_coord.z)
+            .context("reading chunk")?
+            .context("chunk not in region file")?;
+        let chunk = Chunk::parse(relative_coord, absolute_coord, &data)?;
+        self.save_chunk(&f(chunk)?)?;
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip_all, fields(self.path = %self.path, self.coord = %self.coord, absolute_coord = %absolute_coord))]
+    pub fn remove_chunk(&mut self, absolute_coord: Coord<i64>) {
+        let relative_coord = make_relative(self.coord, absolute_coord)?;
+        self.region
+            .remove_chunk(relative_coord.x, relative_coord.z)
+            .context("removing chunk")?;
+    }
+
+    #[tracing::instrument(skip_all, fields(self.path = %self.path, self.coord = %self.coord))]
+    pub fn chunks(&mut self) -> impl Iterator<Item = Result<Coord<i64>>> + '_ {
+        self.region.iter().map(|chunk| {
+            let chunk = chunk.context("reading chunk")?;
+            let relative_coord = Coord {
+                x: chunk.x,
+                z: chunk.z,
+            };
+            Ok(make_absolute(self.coord, relative_coord)?)
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Chunk {
+    pub relative_coord: Coord<usize>,
+    pub absolute_coord: Coord<i64>,
+    data: Compound,
+}
+
+impl Chunk {
+    #[culpa::throws]
+    #[tracing::instrument(skip(data), fields(relative_coord = %relative_coord, absolute_coord = %absolute_coord))]
+    fn parse(relative_coord: Coord<usize>, absolute_coord: Coord<i64>, data: &[u8]) -> Self {
+        Self {
+            relative_coord,
+            absolute_coord,
+            data: fastnbt::from_bytes(data).context("parsing chunk")?,
+        }
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip(self), fields(self.relative_coord = %self.relative_coord))]
+    fn serialize(&self) -> Vec<u8> {
+        fastnbt::to_bytes(&self.data)?
+    }
+
+    #[culpa::throws]
+    #[tracing::instrument(skip(self), fields(self.relative_coord = %self.relative_coord))]
+    pub fn force_blending(mut self) -> Self {
+        // ensure!(self.data.get("Status") == Some(&fastnbt::Value::String("minecraft:full".into())), "chunk is not fully generated");
+        self.data.remove("Heightmaps");
+        self.data.remove("isLightOn");
+        self.data.insert(
+            "blending_data".into(),
+            fastnbt::nbt!({
+                "min_section": -4,
+                "max_section": 20,
+            }),
+        );
+        self
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,165 @@
+#![allow(clippy::needless_question_mark)] // stupid lint
+
+use camino::Utf8PathBuf;
+use clap::Parser;
+use eyre::{bail, ContextCompat, Error};
+use std::collections::{BTreeMap, BTreeSet};
+use tracing_subscriber::layer::SubscriberExt;
+
+mod data;
+
+use data::{Coord, World};
+
+#[derive(Debug, clap::Parser)]
+struct Args {
+    /// Path to world directory
+    world: Utf8PathBuf,
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, clap::Parser)]
+enum Command {
+    RandomizeSeed,
+
+    /// Delete all chunks and regions outside this border
+    RemoveChunksOutsideSquareBorder {
+        /// Top-left (most negative x and z) corner chunk
+        #[arg(allow_hyphen_values(true))]
+        tl: Coord<i64>,
+        /// Bottom-right (most positive x and z) corner chunk
+        #[arg(allow_hyphen_values(true))]
+        br: Coord<i64>,
+    },
+
+    ForceBlending {
+        #[command(subcommand)]
+        target: ForceBlendingTarget,
+    },
+}
+
+#[derive(Debug, clap::Parser)]
+enum ForceBlendingTarget {
+    /// Target all chunks
+    All,
+
+    /// Target these chunk coordinates
+    Chunks { chunks: Vec<Coord<i64>> },
+
+    /// Target chunks in the border of a square
+    SquareBorder {
+        /// Top-left (most negative x and z) corner
+        #[arg(allow_hyphen_values(true))]
+        tl: Coord<i64>,
+        /// Bottom-right (most positive x and z) corner
+        #[arg(allow_hyphen_values(true))]
+        br: Coord<i64>,
+    },
+}
+
+#[culpa::throws]
+fn main() {
+    color_eyre::install()?;
+
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::fmt()
+            .with_writer(std::io::stderr)
+            // .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ENTER)
+            .compact()
+            .finish()
+            .with(tracing_error::ErrorLayer::default()),
+    )?;
+
+    let args = Args::parse();
+    let world = World::new(args.world);
+
+    match args.command {
+        Command::RandomizeSeed => {
+            world.update_level(|mut level| {
+                let Some(fastnbt::Value::Compound(data)) = level.get_mut("Data") else {
+                    bail!("bad Data")
+                };
+                let Some(fastnbt::Value::Compound(settings)) = data.get_mut("WorldGenSettings")
+                else {
+                    bail!("bad WorldGenSettings")
+                };
+                settings.insert("seed".to_string(), fastnbt::Value::Long(rand::random()));
+                Ok(level)
+            })?;
+        }
+
+        Command::RemoveChunksOutsideSquareBorder { tl, br } => {
+            let tlr = tl.chunk_to_region();
+            let brr = br.chunk_to_region();
+            let kept_regions = BTreeSet::from_iter(
+                ((tlr.x)..=(brr.x)).flat_map(|x| ((tlr.z)..=(brr.z)).map(move |z| Coord { x, z })),
+            );
+            let all_regions = Result::<BTreeSet<_>, _>::from_iter(
+                world.regions()?.map(|r| Ok::<_, Error>(r?.coord)),
+            )?;
+            let deleted_regions = &all_regions - &kept_regions;
+            for coord in deleted_regions {
+                world.remove_region(coord)?;
+            }
+            let deleted_chunks =
+                ((tlr.x << 5)..(tl.x))
+                    .flat_map(|x| ((tlr.z << 5)..((brr.z + 1) << 5)).map(move |z| Coord { x, z }))
+                    .chain(((tlr.z << 5)..(tl.z)).flat_map(|z| {
+                        ((tlr.x << 5)..((brr.x + 1) << 5)).map(move |x| Coord { x, z })
+                    }))
+                    .chain(((br.x + 1)..((brr.x + 1) << 5)).flat_map(|x| {
+                        ((tlr.z << 5)..((brr.z + 1) << 5)).map(move |z| Coord { x, z })
+                    }))
+                    .chain(((br.z + 1)..((brr.z + 1) << 5)).flat_map(|z| {
+                        ((tlr.x << 5)..((brr.x + 1) << 5)).map(move |x| Coord { x, z })
+                    }));
+
+            let mut deleted_chunk_map = BTreeMap::<Coord<i64>, BTreeSet<Coord<i64>>>::new();
+            for coord in deleted_chunks {
+                deleted_chunk_map
+                    .entry(coord.chunk_to_region())
+                    .or_default()
+                    .insert(coord);
+            }
+            for (region_coord, chunks) in deleted_chunk_map {
+                if let Some(mut region) = world.region(region_coord)? {
+                    for chunk_coord in chunks {
+                        region.remove_chunk(chunk_coord)?;
+                    }
+                }
+            }
+        }
+
+        Command::ForceBlending { target } => {
+            let coords = match target {
+                ForceBlendingTarget::All => {
+                    let mut chunks = Vec::new();
+                    for region in world.regions()? {
+                        for chunk in region?.chunks() {
+                            chunks.push(chunk?);
+                        }
+                    }
+                    chunks
+                }
+                ForceBlendingTarget::Chunks { chunks } => chunks,
+                ForceBlendingTarget::SquareBorder { tl, br } => {
+                    Vec::from_iter(BTreeSet::from_iter(
+                        ((tl.x)..=(br.x))
+                            .flat_map(|x| [Coord { x, ..tl }, Coord { x, ..br }])
+                            .chain(
+                                ((tl.z)..=(br.z))
+                                    .flat_map(|z| [Coord { z, ..tl }, Coord { z, ..br }]),
+                            ),
+                    ))
+                }
+            };
+
+            for coord in coords {
+                world
+                    .region_for_chunk(coord)?
+                    .context("missing region")?
+                    .update_chunk(coord, |chunk| Ok(chunk.force_blending()?))?
+            }
+        }
+    }
+}
